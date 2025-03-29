a6643e62f4764eebdc0e8d79afddf50e729da9bc
w.alexander <will_alexander@me.com>
2025-03-29 23:05:36 +0100

    Add components

---
+++ src/app/housing/components/make-offer/make-offer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-make-offer',
+  imports: [],
+  template: `
+    <p>
+      make-offer works!
+    </p>
+  `,
+  styles: ``
+})
+export class MakeOfferComponent {
+
+}
---
+++ src/app/housing/components/make-offer/offer-made-modal/offer-made-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-made-modal',
+  imports: [],
+  template: `
+    <p>
+      offer-made-modal works!
+    </p>
+  `,
+  styles: ``
+})
+export class OfferMadeModalComponent {
+
+}
