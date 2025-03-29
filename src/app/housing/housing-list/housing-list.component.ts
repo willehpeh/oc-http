acd722521eab158c4bdbc2f8488c3607645312e0
w.alexander <will_alexander@me.com>
2025-03-29 22:05:39 +0100

    Add housing list component

---
+++ src/app/housing/housing-list/housing-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-list',
+  imports: [],
+  template: `
+    <p>
+      housing-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class HousingListComponent {
+
+}
