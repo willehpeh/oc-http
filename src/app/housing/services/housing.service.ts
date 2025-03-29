df8ac52283768c96ebbec1139c98ec3a56c65e9e
w.alexander <will_alexander@me.com>
2025-03-29 22:43:26 +0100

    Add individual get method to service

--- src/app/housing/services/housing.service.ts
+++ src/app/housing/services/housing.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { HousingPropertyPreview } from '../models/housing-property';
+import { HousingPropertyPreview, HousingPropertyWithDetails } from '../models/housing-property';
 import { DUMMY_PROPERTIES } from '../test-data/DUMMY_PROPERTIES';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
+
   getHousingPropertiesList(): Observable<HousingPropertyPreview[]> {
     return of(DUMMY_PROPERTIES);
+  }
+
+  getHousingProperty(id: string): Observable<HousingPropertyWithDetails> {
+    const found = DUMMY_PROPERTIES.find(property => property.id === id);
+    if (!found) {
+      throw new Error(`Property with id ${id} not found`);
+    }
+    return of(found);
   }
 }
