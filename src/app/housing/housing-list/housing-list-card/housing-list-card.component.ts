5c00b63ced9009c0c45677fc674013534f043e96
w.alexander <will_alexander@me.com>
2025-03-29 22:08:00 +0100

    Add property grid element

---
+++ src/app/housing/housing-list/housing-list-card/housing-list-card.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-list-card',
+  imports: [],
+  template: `
+    <p>
+      housing-list-card works!
+    </p>
+  `,
+  styles: ``
+})
+export class HousingListCardComponent {
+
+}
--- src/app/housing/housing-list/housing-list.component.ts
+++ src/app/housing/housing-list/housing-list.component.ts
@@ -4,11 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-housing-list',
   imports: [],
   template: `
-    <p>
-      housing-list works!
-    </p>
+		<main class="property-list">
+			<div class="property-grid">
+      </div>
+    </main>
   `,
-  styles: ``
+  styles: `
+    .property-grid {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+      gap: 2rem;
+    }
+  `
 })
 export class HousingListComponent {
 
