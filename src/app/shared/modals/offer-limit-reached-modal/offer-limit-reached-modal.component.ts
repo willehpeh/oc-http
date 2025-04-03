492c34c68ab36351df5bf462b9b8107c9927a118
w.alexander <will_alexander@me.com>
2025-04-03 22:36:46 +0200

    Restructure folders

--- src/app/shared/modals/offer-limit-reached-modal/offer-limit-reached-modal.component.ts
+++ src/app/shared/modals/offer-limit-reached-modal/offer-limit-reached-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ModalService } from '../../../../layout/services/modal.service';
+import { ModalService } from '../../../core/layout/services/modal.service';
 import { Router } from '@angular/router';
 
 @Component({
