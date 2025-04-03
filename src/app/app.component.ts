a8c51c3fdd6816f1855b7a2285081416628d6001
w.alexander <will_alexander@me.com>
2025-04-03 22:36:46 +0200

    Restructure folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
+import { HeaderComponent } from './core/layout/header/header.component';
 
 @Component({
   selector: 'app-root',
