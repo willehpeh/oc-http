9224218ea0757022e1256893409e8719b44feaa7
w.alexander <will_alexander@me.com>
2025-04-05 23:25:47 +0200

    Add token service

---
+++ src/app/core/auth/token.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  private fakeToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+  userToken(): string {
+    return this.fakeToken;
+  }
+}
